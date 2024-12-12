46d1ca38e6f3026352a8e012b9043a7b0abdf59d
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-12 17:21:55 +0500

    ishga tushuruvchi script yaxshilandi

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 
-uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --reload --reload-dir core --reload-dir config
-
+# uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --reload --reload-dir core --reload-dir config
+gunicorn config.wsgi:application -b 0.0.0.0:8000 --workers 8
 
 
 exit $?
