69efd2caad4f54aa239c265bdcb72c3cf4498552
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-12 17:38:06 +0500

    entrypoint.sh updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 
 # uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --reload --reload-dir core --reload-dir config
-gunicorn config.wsgi:application -b 0.0.0.0:8000 --workers 9
+gunicorn config.wsgi:application -b 0.0.0.0:8000 --workers $(($(nproc) * 2 + 1))
 
 
 exit $?
