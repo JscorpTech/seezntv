42cca8a873ee88fc4d062c6f5804126f9b6094ae
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-12 17:35:35 +0500

    optimize project

--- config/settings/local.py
+++ config/settings/local.py
@@ -9,7 +9,7 @@ INSTALLED_APPS += [
 ALLOWED_HOSTS += ["127.0.0.1", "192.168.100.26"]
 
 REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {
-    "user": "10/min",
+    "user": "30/min",
 }
 MIDDLEWARE += [
     "silk.middleware.SilkyMiddleware",
