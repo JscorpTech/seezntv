7096eec259965794b0e68695e7084de99fd7b605
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-14 15:31:02 +0500

    Yangi dashboard funksiyasi qo'shildi va statistikalar yangilandi

--- config/conf/unfold.py
+++ config/conf/unfold.py
@@ -1,7 +1,11 @@
 from . import navigation
 
+from django.utils.translation import gettext as _
+from core.apps.api import models
+from django.contrib.auth import get_user_model
+
 UNFOLD = {
-    "DASHBOARD_CALLBACK": "django_core.views.dashboard_callback",
+    "DASHBOARD_CALLBACK": "config.conf.dashboard_callback",
     "SITE_TITLE": None,
     "SHOW_LANGUAGES": True,
     "SITE_HEADER": None,
@@ -39,3 +43,65 @@ UNFOLD = {
         "navigation": navigation.PAGES,  # Pagelarni qo'lda qo'shish
     },
 }
+
+
+def dashboard_callback(request, context):
+    data = {
+        "films": models.ContentModel.objects.count(),
+        "categories": models.CategoryModel.objects.count(),
+        "tags": models.TagModel.objects.count(),
+        "genres": models.GenreModel.objects.count(),
+        "contents": models.ContentModel.objects.count(),
+        "users": get_user_model().objects.count(),
+        "banners": models.BannerModel.objects.count(),
+        "stories": models.IstoryModel.objects.count(),
+    }
+
+    context.update(
+        {
+            "cards": (
+                {
+                    "color": "blue",
+                    "value": data["films"],
+                    "title": _("films"),
+                },
+                {
+                    "color": "primary",
+                    "value": data["categories"],
+                    "title": _("categories"),
+                },
+                {
+                    "color": "orange",
+                    "value": data["tags"],
+                    "title": _("tags"),
+                },
+                {
+                    "color": "red",
+                    "value": data["genres"],
+                    "title": _("genres"),
+                },
+                {
+                    "color": "green",
+                    "value": data["contents"],
+                    "title": _("contents"),
+                },
+                {
+                    "color": "red",
+                    "value": data["users"],
+                    "title": _("users"),
+                },
+                {
+                    "color": "green",
+                    "value": data["banners"],
+                    "title": _("banners"),
+                },
+                {
+                    "color": "green",
+                    "value": data["stories"],
+                    "title": _("stories"),
+                },
+            ),
+        }
+    )
+
+    return context
