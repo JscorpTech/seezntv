a7572b77e55b7ccaa5a55f54ce80d6631708a94e
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-18 12:16:23 +0500

    O'zgartirishlar: .env.example va config/settings/common.py fayllarida bir necha parametrlar yangilandi; auth.py dan unused kod olib tashlandi.

--- config/settings/common.py
+++ config/settings/common.py
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
 if env.str("PROJECT_ENV") == "debug":
     MIDDLEWARE += ["silk.middleware.SilkyMiddleware"]
 
