4ab7ccab55952d5cee51e3b1614aab106ac8f49b
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-19 15:49:21 +0500

    Url ni unquote qilish funksiyasini qo'shildi va foydalanildi

--- core/apps/api/serializers/shared/cadr.py
+++ core/apps/api/serializers/shared/cadr.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 
 from ...models import CadrModel
+from core.utils import Url
 
 
 class BaseCadrSerializer(serializers.ModelSerializer):
+
+    def to_representation(self, instance):
+        return Url.unquote_if_exists(
+            super().to_representation(instance),
+            [
+                "image",
+            ],
+        )
+
     class Meta:
         model = CadrModel
         exclude = [
--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,3 +1,4 @@
 from .cache import *  # noqa
 from .console import *  # noqa
 from .core import *  # noqa
+from .url import *  # noqa
